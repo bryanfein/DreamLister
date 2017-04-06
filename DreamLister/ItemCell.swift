08acf36ef937be949fda2ca53b767c638cfa5d9f
bryanfein <bryanfein@gmail.com>
2017-04-06 14:16:05 -0400

    Implemented UITableView delegates

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Bryan Fein on 4/6/17.
+//  Copyright © 2017 Bryan Fein. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var itemPrice: UILabel!
+    @IBOutlet weak var itemDecription: UILabel!
+    
+    
+    
+}
--- DreamLister/MainViewController.swift
+++ DreamLister/MainViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //make sure you set your delegates in the viewDidLoad
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //make sure you add the required methods for UITableView
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
 }
 
